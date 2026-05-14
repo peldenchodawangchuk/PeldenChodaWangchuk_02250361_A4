172cca2c0e8ab1f81c7308d893d1550d052f7ad1
pelden choda wangchuk <gokupelden@gmail.com>
2026-05-14 10:34:00 +0600

    Readme

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def home():
     <html>
     <head><title>My DevOps App</title></head>
     <body style="font-family: Arial; text-align: center; padding: 50px;">
-        <h1>Hello DevOps! 🚀</h1>
+        <h1>Hello DevOps!</h1>
         <p>This app is deployed using GitHub Actions and Render.</p>
         <p><em>Assignment IV – DSO101 | SWE</em></p>
     </body>
